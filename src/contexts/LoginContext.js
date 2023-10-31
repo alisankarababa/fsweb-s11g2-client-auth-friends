fe72f165fdf461ae66f3594ec72e19a540217b31
Alişan Karababa <alisankarababa@gmail.com>
2023-10-31 13:18:56 +0300

    add LoginContext

---
+++ src/contexts/LoginContext.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { createContext } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+export const LoginContext = createContext();
+
+export default function LoginProvider({ children }) {
+
+    const history = useHistory();
+
+	function hLogin(credentials) {
+		console.log(credentials);
+		axios
+			.post("http://localhost:9000/api/login", credentials)
+			.then(function (response) {
+				console.log(response);
+                localStorage.setItem("token", response.data.token);
+
+
+                history.push("/friends");
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	return (
+		<LoginContext.Provider value={{ hLogin }}>
+			{children}
+		</LoginContext.Provider>
+	);
+}
