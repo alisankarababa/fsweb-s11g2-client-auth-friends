0ab289771c5faf6df7af47ec2df23a900541c6db
Alişan Karababa <alisankarababa@gmail.com>
2023-10-31 13:20:37 +0300

    add page routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import Login from './component/Login';
+import AddFriend from "./component/AddFriend"
+
+import {
+    Switch,
+    Route,
+} from "react-router-dom";
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>Client Auth Projesi: Friends</h1>
+
+    <Switch >
+        <Route exact path="/">
+            <Login className="container"/>
+        </Route>
+        <Route exact path="/login">
+            <Login className="container"/>
+        </Route>
+        <Route exact path="/friends">
+            <div>FRIENDS PAGE</div>
+        </Route>
+    </Switch>
     </div>
+
+    </>
+
   );
 }
 
